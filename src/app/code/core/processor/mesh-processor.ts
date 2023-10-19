f8615eb5d11cbce22919a9689a068a8000106794
Cristian Marcelo de Picciotto <d3p1@d3p1.dev>
2023-10-19 12:57:51 +0000

    refactor: add mesh processor [#1]

---
+++ src/app/code/core/processor/mesh-processor.ts
@@ -0,0 +1,15 @@
+/**
+ * @description Mesh processor
+ * @author      C. M. de Picciotto <d3p1@d3p1.dev> (https://d3p1.dev/)
+ */
+import * as THREE from 'three'
+import AbstractProcessor from '../processor'
+
+export default abstract class MeshProcessor extends AbstractProcessor {
+  /**
+   * @inheritdoc
+   */
+  protected _create(): THREE.Object3D {
+    return new THREE.Mesh(this._geometry, this._material)
+  }
+}
