456b4320b511dea0829499b917b24d62d624bd70
Cristian Marcelo de Picciotto <d3p1@d3p1.dev>
2023-10-19 12:48:20 +0000

    refactor: add base renderer for objects [#1]

---
+++ src/app/code/core/processor.ts
@@ -0,0 +1,45 @@
+/**
+ * @description Object processor
+ * @author      C. M. de Picciotto <d3p1@d3p1.dev> (https://d3p1.dev/)
+ */
+import * as THREE from 'three'
+
+export default abstract class Processor {
+  /**
+   * Constructor
+   *
+   * @param {object} _geometry
+   * @param {object} _material
+   * @param {object} _scene
+   */
+  constructor(
+    protected _geometry: THREE.BufferGeometry,
+    protected _material: THREE.Material,
+    protected _scene: THREE.Scene,
+  ) {
+    this.init()
+  }
+
+  /**
+   * Init object
+   *
+   * @returns {void}
+   */
+  public init(): void {
+    this._scene.add(this._create())
+  }
+
+  /**
+   * Update object
+   *
+   * @returns {void}
+   */
+  public abstract update(): void
+
+  /**
+   * Create object
+   *
+   * @returns {object}
+   */
+  protected abstract _create(): THREE.Object3D
+}
