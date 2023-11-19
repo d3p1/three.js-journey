750de06234c7f06f26118e76b31bf6b455d47639
Cristian Marcelo de Picciotto <cristian.depicciotto@balloon-group.com>
2023-11-19 14:28:59 +0000

    feat: add group processor [#1]

---
+++ src/app/code/core/model/bootstrap/object-processor/group-processor.ts
@@ -0,0 +1,15 @@
+/**
+ * @description Group processor
+ * @author      C. M. de Picciotto <d3p1@d3p1.dev> (https://d3p1.dev/)
+ */
+import * as THREE from 'three'
+import AbstractProcessor from '../object-processor'
+
+export default abstract class GroupProcessor extends AbstractProcessor {
+  /**
+   * @inheritdoc
+   */
+  protected _create(): THREE.Object3D {
+    return new THREE.Group()
+  }
+}
