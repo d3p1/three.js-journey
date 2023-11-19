71fd447a706514419a1e0fe93c3a338c66d8ab83
Cristian Marcelo de Picciotto <cristian.depicciotto@balloon-group.com>
2023-11-19 13:28:02 +0000

    fix: init rubik piece [#1]

---
+++ src/app/topic/rubik/model/object-processor/rubik/mesh/piece.ts
@@ -0,0 +1,29 @@
+/**
+ * @description Rubik piece
+ * @author      C. M. de Picciotto <d3p1@d3p1.dev> (https://d3p1.dev/)
+ */
+import * as THREE from 'three'
+import MeshProcessor from '../../../../../../code/core/model/bootstrap/object-processor/mesh-processor'
+
+export default class Piece extends MeshProcessor {
+  /**
+   * Constructor
+   *
+   * @param {number}                  size
+   * @param {number|string}           color
+   * @param {THREE.MeshBasicMaterial} process
+   * @param {THREE.PlaneGeometry}     geometry
+   * @param {THREE.MeshBasicMaterial} material
+   */
+  constructor(
+    size: number,
+    color: string | number,
+    process: (time: number, obj: THREE.Object3D) => void = () => {},
+    geometry: THREE.PlaneGeometry = new THREE.PlaneGeometry(size, size),
+    material: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial({
+      color: color,
+    }),
+  ) {
+    super(process, geometry, material)
+  }
+}
