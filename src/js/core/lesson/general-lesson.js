dc2d29b1c35371ca30ff6256e35fd6db2dd206e6
Cristian Marcelo de Picciotto <cmdepicciotto@gmail.com>
2024-09-29 15:28:11 -0300

    feat: init general lesson class [#1]

---
+++ src/js/core/lesson/general-lesson.js
@@ -0,0 +1,40 @@
+/**
+ * @description General lesson class
+ * @author      C. M. de Picciotto <d3p1@d3p1.dev> (https://d3p1.dev/)
+ */
+import Lesson from '../lesson.js'
+
+export default class GeneralLesson extends Lesson {
+  /**
+   * @type {HTMLCanvasElement}
+   */
+  canvas
+
+  /**
+   * Constructor
+   */
+  constructor() {
+    super()
+
+    this.#initCanvas()
+  }
+
+  /**
+   * Init canvas
+   *
+   * @returns {void}
+   */
+  #initCanvas() {
+    this.canvas = document.createElement('canvas')
+    document.appendChild(this.canvas)
+  }
+
+  /**
+   * Close lesson
+   *
+   * @returns {void}
+   */
+  close() {
+    document.removeChild(this.canvas)
+  }
+}
