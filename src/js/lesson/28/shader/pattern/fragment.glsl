73bd650a4307ccf8e01cd13ceada3499290da69f
Cristian Marcelo de Picciotto <cmdepicciotto@gmail.com>
2024-11-05 19:04:00 -0300

    feat: create plus pattern for lesson number 28 [#1]

--- src/js/lesson/28/shader/pattern/fragment.glsl
+++ src/js/lesson/28/shader/pattern/fragment.glsl
@@ -3,13 +3,16 @@
 varying vec2 vUv;
 
 void main() {
-    float clipX     = mod(vUv.x * 10.0, 1.0);
+    float x = vUv.x * 10.0;
+    float y = vUv.y * 10.0;
+
+    float clipX     = mod(x + 0.2, 1.0);
     float strengthX = step(0.8, clipX);
-    float clipY     = mod(vUv.y * 10.0, 1.0);
+    float clipY     = mod(y + 0.2, 1.0);
     float strengthY = step(0.8, clipY);
 
-    float barX = step(0.4, clipY);
-    float barY = step(0.4, clipX);
+    float barX = step(0.4, mod(y, 1.0));
+    float barY = step(0.4, mod(x, 1.0));
 
     float strength = (strengthX * barX) + (strengthY * barY);
 
